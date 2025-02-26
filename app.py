34a9a200a8c36feb175e027c1a7dce32e03a15d7
youssoupha-marega <youssouphamarega@gmail.com>
2025-02-26 14:32:50 -0500

    Probleme port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import pickle
 
+
 from flask import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
 import pandas as pd
@@ -33,7 +34,10 @@ def predict():
 
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
    
      
