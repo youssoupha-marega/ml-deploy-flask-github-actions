fc952c82469a43839976981bcc64a96f327e4a59
youssoupha-marega <youssouphamarega@gmail.com>
2025-03-10 14:40:55 -0400

    test paquet lourd

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask,  jsonify
+from transformers import MgpstrProcessor, MgpstrForSceneTextRecognition
+from torch.nn.functional import relu
 
+import torch
+import pkg_resources
+import flask
+import PIL
+
+print("Flask version:", flask.__version__)
+print("Pillow version:", PIL.__version__)
+
+
+print(torch.__version__)
+
+
+
+
+### je veux afficher les versions de mon requirement.txt
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! good'
+    return 'Hello, World! good 111'
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+@app.route('/versions')
+def get_versions():
+    # Lire le fichier requirements.txt
+    with open("requirements.txt", "r") as f:
+        required_packages = [line.strip().split("==")[0] for line in f.readlines() if "==" in line]
+
+    # Récupérer les versions installées
+    installed_packages = {pkg.key: pkg.version for pkg in pkg_resources.working_set}
+
+    # Filtrer uniquement les packages présents dans requirements.txt
+    filtered_versions = {pkg: installed_packages.get(pkg, "Not Installed") for pkg in required_packages}
+
+    return jsonify(filtered_versions)
 
 if __name__ == '__main__':
     app.run()
